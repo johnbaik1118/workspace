45bded7d41186a5ee33d3670ae6d04d8ab3783c1
johnbaik1118 <johnbaik1118@naver.com>
2018-04-09 14:35:02 +0900

    malloc.c

--- hotpatch/dummy/arm/malloc.c
+++ hotpatch/dummy/arm/malloc.c
@@ -6,4 +6,9 @@ SET_REGS(regs, r0, key);
 SET_REGS(regs, r1, size);
 SET_REGS(regs, r2, flag);
 SET_REGS(regs, r3, pid);
-SET_REGS(regs, r7,
+SET_REGS(regs, r7, __NR_SHMGET);
+SET_REGS(regs, pc, regs.uregs[sp]-8);
+
+ptrace(PTRACE_SETREGS, pid, NULL, &regs);
+ptrace(PTRACE_POKEDATA, pid, regs.uregs[sp]-8, syscall_ins);
+ptrace(PTRACE_CONT, pid, NULL, NULL);
